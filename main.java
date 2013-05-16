5056bc26b46ace99d508af26b4900a81a0c3bb59
Klaas <boedefeld@outlook.com>
2013-05-16 17:07:25 +0200

    Prompted the user for the lastname as well.

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ class HelloWorld{
 	public static void main(String[] args) throws IOException{
 		String CurLine = ""; // Line read from standard in
 		
-		System.out.println("Enter your name (type 'quit' to exit): ");
+		System.out.println("Enter your name and lastname (type 'quit' to exit): ");
 		InputStreamReader converter = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(converter);
 		System.out.println();
