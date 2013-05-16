69c644ddf8eab0ed658aa6187d3b54387f3a0619
Klaas <boedefeld@outlook.com>
2013-05-16 17:03:52 +0200

    give a personalized hello

--- main.java
+++ main.java
@@ -1,7 +1,19 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-
+import java.io.*;
+class HelloWorld{
+	public static void main(String[] args) throws IOException{
+		String CurLine = ""; // Line read from standard in
+		
+		System.out.println("Enter your name (type 'quit' to exit): ");
+		InputStreamReader converter = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(converter);
+		System.out.println();
+		
+		while (!(CurLine.equals("quit"))){
+			CurLine = in.readLine();
+			
+			if (!(CurLine.equals("quit"))){
+				System.out.println("Hello " + CurLine);
+			}
+		}
+	}
 }
